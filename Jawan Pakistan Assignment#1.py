bbf03af8e9c7999c8d3328bbcc3a9bf0540309b6
Somer0801 <61990861+Somer0801@users.noreply.github.com>
2022-01-01 23:44:36 +0500

    Add files via upload

---
+++ Jawan Pakistan Assignment#1.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Program#1 
+# Write a Python program to print the following string in a specific format (see the output).
+print(f"Twinkle, twinkle, little star,\n        How I wonder what you are! \n              Up above the world so high, \n              Like a diamond in the sky. \n Twinkle, twinkle, little star,\n          How I wonder what you are")
+
+
+# In[ ]:
+
+
+# Program#2
+# Write a Python program to get the Python version you are using.
+import sys
+print(sys.version)
+
+
+# In[1]:
+
+
+# Program#3
+# Write a Python program to display the current date and time.
+from datetime import datetime
+now = datetime.now()
+print (now.strftime("Date : %d-%m-%y \nTime : %H:%M:%S"))
+
+
+# In[ ]:
+
+
+# Program#4
+# Write a Python program which accepts the radius of a circle from the user and compute the area.
+Radius = int(input("Enter your radius : "))
+Area = (3.142)*Radius**2
+print(f"The area of your circle is {Area}")
+
+
+# In[ ]:
+
+
+#Program#5
+# Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+first_Name=input("Enter your first name : ")
+Last_Name=input(" Enter your Last name : ")
+x=" "
+Name=(first_Name)+x+(Last_Name)
+print(Name[: :-1])
+
+
+# In[ ]:
+
+
+# Program#6
+# Write a python program which takes two inputs from user and print them addition
+First_num=int(input("Enter first number : "))
+Second_num=int(input("Enter second number : "))
+Add= First_num+Second_num
+print(f"Your Sum is {Add}")
+
+
+# In[ ]:
+
+
+#
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# P
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
